e25bf8a4a883b761ff1d911dd580dc0cf39014c3
Codescandy <codescandy@192.168.1.3>
2023-04-22 15:13:57 +0530

    dist folder build with asstes inner folder

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
+import {defineConfig} from 'vite';
 const path = require('path')
 
-export default {
+export default defineConfig({
   base: './',
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+        
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+              return 'assets/images/[name][extname]';
+          }
+          
+          if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name][extname]';   
+          }
+ 
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    }
   },
   server: {
     port: 8080
   }
-}
+})
